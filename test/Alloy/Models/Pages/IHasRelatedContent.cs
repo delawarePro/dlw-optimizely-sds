83dfd2314f6ef78ed2386d65fee03d509f0a8ede
Jeroen Debruyne <Jeroen.Debruyne+dlwr@delaware.pro>
2026-05-18 11:26:06 +0200

    Add Alloy template site with central package management

---
+++ test/Alloy/Models/Pages/IHasRelatedContent.cs
@@ -0,0 +1,6 @@
+namespace Alloy.Models.Pages;
+
+public interface IHasRelatedContent
+{
+    public ContentArea RelatedContentArea { get; }
+}
---
+++ test/Alloy/Models/ViewModels/PageListModel.cs
@@ -0,0 +1,14 @@
+using Alloy.Models.Blocks;
+
+namespace Alloy.Models.ViewModels;
+
+public class PageListModel(PageListBlock block)
+{
+    public string Heading { get; set; } = block.Heading;
+
+    public IEnumerable<PageData> Pages { get; set; }
+
+    public bool ShowIntroduction { get; set; } = block.IncludeIntroduction;
+
+    public bool ShowPublishDate { get; set; } = block.IncludePublishDate;
+}
